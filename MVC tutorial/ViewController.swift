9028e812a8d006e473fee99770524751a065fbe5
Mostafa Elshazly <mostafa.elshazly.000@gmail.com>
2020-09-10 08:09:15 +0200

    Initial Commit

---
+++ MVC tutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVC tutorial
+//
+//  Created by Mostafa Elshazly on 9/10/20.
+//  Copyright © 2020 Mostafa Elshazly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
